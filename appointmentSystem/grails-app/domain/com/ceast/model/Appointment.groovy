73e961b125a1fa2271dccba02632e017cea489c5
callum753 <c0013851@hallam.shu.ac.ukls>
2023-05-09 12:23:17 +0000

    updated 2 of the grove

--- appointmentSystem/grails-app/domain/com/ceast/model/Appointment.groovy
+++ appointmentSystem/grails-app/domain/com/ceast/model/Appointment.groovy
@@ -2,6 +2,18 @@ package com.ceast.model
 
 class Appointment {
 
+    AppDateTime dateTime
+    AppDuration int 
+    RoomNumber string
+
+
+
     static constraints = {
+        Class_()
+        Appdatatime()
+        AppDuration(min:15,max:60)
+        RoomNumber()
+
+
     }
 }
--- appointmentSystem/grails-app/domain/com/ceast/model/Surgery.groovy
+++ appointmentSystem/grails-app/domain/com/ceast/model/Surgery.groovy
@@ -1,7 +1,24 @@
 package com.ceast.model
 
 class Surgery {
+    Name string
+    Address string
+    Postcode string 
+    Telphone string 
+    NumberofPatients character
+    Description string
+    OpeningTimes string
+    NewPatient Boolean 
+
 
     static constraints = {
+        Name()
+        Address()
+        Postcode()
+        Telphone ()
+        NumberofPatients()
+        Description()
+        OpeningTimes()
+        NewPatient()
     }
 }
